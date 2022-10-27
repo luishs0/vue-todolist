bf37f328d7d27508f26edd25f9d457e13ad8beea
luishs0 <luishs.doc@gmail.com>
2022-10-27 15:22:21 +0200

    milestone 1

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const {createApp} = Vue;
+
+createApp({
+
+    data() {
+
+        return {
+            tasks: [
+                {
+                    title: "task 1",
+                    done: false,
+                },
+                {
+                    title: "task 2",
+                    done: false,
+                }
+            ]
+        }
+
+    },
+
+    methods: {
+       isDone(index) {
+            if (this.tasks[index].done === false) {
+                this.tasks[index].done = true;
+            } else if (this.tasks[index].done === true) {
+                this.tasks[index].done = false;
+            }
+       }
+    }
+
+}).mount('#app');
